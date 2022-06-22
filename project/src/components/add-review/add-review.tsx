ebe8332dd847b2b69960af167783a21a9d68d0c6
kotser1 <newelvin@gmail.com>
2022-06-22 16:41:16 +0300

    feat: create review form

--- project/src/components/add-review/add-review.tsx
+++ project/src/components/add-review/add-review.tsx
@@ -1,10 +1,6 @@
-import { Fragment, useState, ChangeEvent } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import { Film } from '../../types/films';
-
-const MAX_RATING = 10;
-
-const ratings = Array.from({length: MAX_RATING}, (v, k) => k + 1).reverse();
+import ReviewForm from '../review-form/review-form';
 
 type AddReviewProps = {
   films: Film[];
@@ -14,17 +10,6 @@ function AddReview({films}: AddReviewProps): JSX.Element {
   const params = useParams();
   const currentId = Number(params.id);
   const currentFilm = films.filter((item) => item.id === currentId)[0];
-
-  const [, setRatingData] = useState<number | null>(null);
-  const [commentData, setCommentData] = useState('');
-
-  const handleRatingChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setRatingData(Number(e.target.value));
-  };
-
-  const handleCommentChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
-    setCommentData(e.target.value);
-  };
 
   return (
     <section className='film-card film-card--full'>
@@ -92,44 +77,7 @@ function AddReview({films}: AddReviewProps): JSX.Element {
       </div>
 
       <div className='add-review'>
-        <form action='#' className='add-review__form'>
-          <div className='rating'>
-            <div className='rating__stars'>
-              {ratings.map((item) => (
-                <Fragment key={item}>
-                  <input
-                    className='rating__input'
-                    id={`star-${item}`}
-                    type='radio'
-                    name='rating'
-                    value={item}
-                    onChange={handleRatingChange}
-                  />
-                  <label className='rating__label' htmlFor={`star-${item}`}>
-                    Rating {item}
-                  </label>
-                </Fragment>
-              ))}
-            </div>
-          </div>
-
-          <div className='add-review__text'>
-            <textarea
-              className='add-review__textarea'
-              name='review-text'
-              id='review-text'
-              placeholder='Review text'
-              value={commentData}
-              onChange={handleCommentChange}
-            >
-            </textarea>
-            <div className='add-review__submit'>
-              <button className='add-review__btn' type='submit'>
-                Post
-              </button>
-            </div>
-          </div>
-        </form>
+        <ReviewForm />
       </div>
     </section>
   );
